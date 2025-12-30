54d5656f1e32d90c2297d5f1d2cc9bbe1f5f33e8
Nathan Nkweto <nathannkweto@nkweto.tech>
2025-12-31 00:17:16 +0100

    chore: update cors policy for testing frontend

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,21 @@
 <?php
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-   */
-
+    | The 'paths' must match the URL pattern of your API.
+    */
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    /*
+    | FOR TESTING
+    | GitHub Pages origin.
+    */
+    'allowed_origins' => [
+        'https://nathannkweto.github.io',
+        'http://localhost:5000',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +26,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
-
 ];
