d143bc901c28af19f217a13ca66b3d0e6cc40a0e
Nathan Nkweto <nathannkweto@nkweto.tech>
2025-12-31 01:27:26 +0100

    fix: configure apache for cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     /*
     | The 'paths' must match the URL pattern of your API.
     */
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
